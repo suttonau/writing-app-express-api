83de7d29c1ff1813aaaa331a3ee01910a11c09e4
Sean Sutton <sutton_mjbc@icloud.com>
2020-08-24 15:52:32 -0400

    adding apollo server

---
+++ index.js
@@ -0,0 +1,38 @@
+const { ApolloServer, gql } = require('apollo-server');
+const { Prompt, sequelize } = require('./models');
+
+const port = process.env.PORT || 4000;
+
+// Schema Definition Language (SDL)
+// in graphql you have to define your schema
+
+// typeDefs, define what types of data are available in the graphql server
+const typeDefs = gql`
+  type Prompt { 
+    title: String!, 
+    id: ID! 
+  }
+  type Query {
+    prompt: Prompt
+  }
+`;
+
+// Resolver Map
+// Execute logic based on the backend models
+const resolvers = {
+  Query: {
+    prompt: async (_, args) => {
+      const prompt = await Prompt.findOne({
+        order: sequelize.random()
+       });
+       return prompt
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+server.listen({port}, () => console.log(`Server running on localhost: ${port}!`))
